5ebb2d35d01a060d98c38742350da2f8e60fead6
divyanshu-02 <66309422+divyanshu-02@users.noreply.github.com>
2020-07-22 13:44:48 +0530

    Create merge_sort.cpp

---
+++ merge_sort.cpp
@@ -0,0 +1,72 @@
+#include <iostream> 
+using namespace std; 
+
+void merge(int a[],int l,int mid,int h)
+{
+    int i,j,k;
+    int n1=mid-l+1;
+    int n2=h-mid;
+    int a3[n1+n2];
+    int a1[n1],a2[n2];
+    for (i = 0; i < n1; i++) 
+        a1[i] = a[l + i]; 
+    for (j = 0; j < n2; j++) 
+        a2[j] = a[mid + 1 + j];
+    i=0;
+    j=0;
+    k=l;
+    while(i<n1&&j<n2)
+    {
+        if(a1[i]<a2[j])
+        {
+            a[k]=a1[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            a[k]=a2[j];
+            j++;
+            k++;
+        }
+    }
+    while(i<n1)
+    {
+        a[k]=a1[i];
+        k++;
+        i++;
+    }
+    while(j<n2)
+    {
+        a[k]=a2[j];
+        k++;
+        j++;
+    }
+}
+void mergesort(int a[],int l,int h)
+{
+    if(l<h)
+    {
+        int mid=(l+h)/2;
+        mergesort(a,l,mid);
+        mergesort(a,mid+1,h);
+        merge(a,l,mid,h);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements : ";
+    cin>>n;
+    int i,a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    mergesort(a,0,n-1);
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<";";
+    }
+    return 0;
+}
